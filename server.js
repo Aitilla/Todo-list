ef17e34b9d5d141aea1c07804a35b74818a231a9
Joachim <jhertaas@gmail.com>
2024-03-20 10:44:51 +0100

    ingore

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 // Start the server
-const port = 3000; // You can use any port you like
+const port = 8080; // You can use any port you like
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
